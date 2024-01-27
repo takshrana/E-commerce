54f584eeea8d5a9c765442ed145eda2d14da4a8b
Taksh <ranataksh03@gmail.com>
2024-01-28 00:17:33 +0530

    added home page structure

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    from app.home import bp as home_bp
+    app.register_blueprint(home_bp)
+
+    app.add_url_rule('/', endpoint='index')
+
     @app.route('/test')
     def test_page():
         return '<h1>Test</h1>'
