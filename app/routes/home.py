18cec1cd5d5366fff9a9a5090dfc9240141598e2
Taksh Rana <ranataksh03@gmail.com>
2024-02-21 23:01:16 +0530

    added product display page

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app.home import bp
+from app.extensions import db
+from app.models.product import Product
 from app.routes.product.update import get_active_product
 
 
@@ -7,3 +9,8 @@ from app.routes.product.update import get_active_product
 def index():
     products = get_active_product()
     return render_template('index.html', products=products)
+
+@bp.route('/product/<int:p_id>')
+def display_product(p_id):
+    product = db.session.execute(db.select(Product).filter_by(id=p_id)).scalar()
+    return render_template('product/product-page.html', product=product)
